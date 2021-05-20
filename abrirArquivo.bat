1cd03c0b39a5997790c0ea741faa6db9db2bb1f2
MarcosAur <marcosaureliomendesoliveira@gmail.com>
2021-05-20 18:11:30 -0300

    Criação do inializador para windows

---
+++ abrirArquivo.bat
@@ -0,0 +1,2 @@
+java -jar --module-path %JAVA_TO_FX% --add-modules javafx.controls,javafx.fxml ./dist/.RestauranteBomPrato.jar
+
--- abrirArquivo.sh
+++ abrirArquivo.sh
@@ -1,1 +1,1 @@
-java -jar --module-path ./libFx --add-modules javafx.controls,javafx.fxml ./dist/.RestauranteBomPrato.jar
+java -jar --module-path .\libFx\javafx-sdk-11.0.2\lib --add-modules javafx.controls,javafx.fxml ./dist/.RestauranteBomPrato.jar
